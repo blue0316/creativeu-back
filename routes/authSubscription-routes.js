0e24fd7a27ec94e46bb64e85653b00f9c953696d
abdul basit <muhammad.zubair3@hsrw.org>
2023-01-24 18:47:45 +0500

    removed validation

--- routes/authSubscription-routes.js
+++ routes/authSubscription-routes.js
@@ -5,133 +5,155 @@ const ApiControllers = require("authorizenet").APIControllers;
 var utils = require("../utils/auth-utils.js");
 
 module.exports = function (app) {
-  app.post("/create-subscription-authorize", (req, res) => {
-    var plan = "";
-    if (req.body.planType === "monthly") {
-      plan = 1;
-    } else if (req.body.planType === "yearly") {
-      plan = 12;
-    } else {
-      plan = 12;
-    }
-    const validationErrors = validateForm(req);
-    if (validationErrors.length > 0) {
-      res.json({ errors: validationErrors });
-      return;
-    }
-    var merchantAuthenticationType =
-      new ApiContracts.MerchantAuthenticationType();
-    merchantAuthenticationType.setName(loginId);
-    merchantAuthenticationType.setTransactionKey(transactionKey);
+	app.post("/create-subscription-authorize", (req, res) => {
+		var plan = "";
+		if (req.body.planType === "monthly") {
+			plan = 1;
+		} else if (req.body.planType === "yearly") {
+			plan = 12;
+		} else {
+			plan = 12;
+		}
+		const validationErrors = validateForm(req);
+		// if (validationErrors.length > 0) {
+		//   res.json({ errors: validationErrors });
+		//   return;
+		// }
+		var merchantAuthenticationType =
+			new ApiContracts.MerchantAuthenticationType();
+		merchantAuthenticationType.setName(loginId);
+		merchantAuthenticationType.setTransactionKey(transactionKey);
 
-    var interval = new ApiContracts.PaymentScheduleType.Interval();
-    interval.setLength(plan);
-    // yearly
-    // monthly
-    // lifetime
-    interval.setUnit(ApiContracts.ARBSubscriptionUnitEnum.MONTHS);
+		var interval = new ApiContracts.PaymentScheduleType.Interval();
+		interval.setLength(plan);
+		// yearly
+		// monthly
+		// lifetime
+		interval.setUnit(ApiContracts.ARBSubscriptionUnitEnum.MONTHS);
 
-    var paymentScheduleType = new ApiContracts.PaymentScheduleType();
-    paymentScheduleType.setInterval(interval);
-    paymentScheduleType.setStartDate(utils.getDate());
-    paymentScheduleType.setTotalOccurrences(5);
-    paymentScheduleType.setTrialOccurrences(0);
+		var paymentScheduleType = new ApiContracts.PaymentScheduleType();
+		paymentScheduleType.setInterval(interval);
+		paymentScheduleType.setStartDate(utils.getDate());
+		paymentScheduleType.setTotalOccurrences(5);
+		paymentScheduleType.setTrialOccurrences(0);
 
-    var creditCard = new ApiContracts.CreditCardType();
-    // creditCard.setExpirationDate("2038-12");
-    creditCard.setExpirationDate(req.body.expire);
-    creditCard.setCardNumber(req.body.cc);
-    // creditCard.setCardNumber("4242424242424242");
+		var creditCard = new ApiContracts.CreditCardType();
+		// creditCard.setExpirationDate("2038-12");
+		creditCard.setExpirationDate(req.body.expire);
+		creditCard.setCardNumber(req.body.cc);
+		// creditCard.setCardNumber("4242424242424242");
 
-    var payment = new ApiContracts.PaymentType();
-    payment.setCreditCard(creditCard);
+		var payment = new ApiContracts.PaymentType();
+		payment.setCreditCard(creditCard);
 
-    var orderType = new ApiContracts.OrderType();
-    orderType.setInvoiceNumber(utils.getRandomString("Inv:1234"));
-    orderType.setDescription(
-      `Your ${req.body.planType} plan has been subscribed.`
-    );
+		var orderType = new ApiContracts.OrderType();
+		orderType.setInvoiceNumber(utils.getRandomString("Inv:1234"));
+		orderType.setDescription(
+			`Your ${req.body.planType} plan has been subscribed.`
+		);
 
-    var customer = new ApiContracts.CustomerType();
-    customer.setType(ApiContracts.CustomerTypeEnum.INDIVIDUAL);
-    customer.setId("customerid_" + utils.getRandomString("Id"));
-    customer.setEmail(req.body.email?req.body.email:req.body.user.email);
-    customer.setPhoneNumber(req.body.cell?req.body.cell:req.body.user.cell);
+		var customer = new ApiContracts.CustomerType();
+		customer.setType(ApiContracts.CustomerTypeEnum.INDIVIDUAL);
+		customer.setId("customerid_" + utils.getRandomString("Id"));
+		customer.setEmail(req.body.email ? req.body.email : req.body.user.email);
+		customer.setPhoneNumber(req.body.cell ? req.body.cell : req.body.user.cell);
 
-    var nameAndAddressType = new ApiContracts.NameAndAddressType();
-    nameAndAddressType.setFirstName(req.body.fname?req.body.fname:req.body.user.fname);
-    nameAndAddressType.setLastName(req.body.lname?req.body.lname:req.body.user.lname);
-    nameAndAddressType.setCompany(req.body.category?req.body.category:req.body.user.category);
-    nameAndAddressType.setAddress(req.body.streetAddressLine1?req.body.streetAddressLine1:req.body.user.streetAddressLine1);
-    nameAndAddressType.setCity(req.body.city?req.body.city:req.body.user.city);
-    nameAndAddressType.setState(req?.body?.stateOrProvince?req?.body?.stateOrProvince:req?.body?.user?.stateOrProvince);
-    nameAndAddressType.setZip(req.body.postalCode?req.body.postalCode:req.body.user.postalCode);
-    nameAndAddressType.setCountry(req.body.country?req.body.country:req.body.user.country);
+		var nameAndAddressType = new ApiContracts.NameAndAddressType();
+		nameAndAddressType.setFirstName(
+			req.body.fname ? req.body.fname : req.body.user.fname
+		);
+		nameAndAddressType.setLastName(
+			req.body.lname ? req.body.lname : req.body.user.lname
+		);
+		nameAndAddressType.setCompany(
+			req.body.category ? req.body.category : req.body.user.category
+		);
+		nameAndAddressType.setAddress(
+			req.body.streetAddressLine1
+				? req.body.streetAddressLine1
+				: req.body.user.streetAddressLine1
+		);
+		nameAndAddressType.setCity(
+			req.body.city ? req.body.city : req.body.user.city
+		);
+		nameAndAddressType.setState(
+			req?.body?.stateOrProvince
+				? req?.body?.stateOrProvince
+				: req?.body?.user?.stateOrProvince
+		);
+		nameAndAddressType.setZip(
+			req.body.postalCode ? req.body.postalCode : req.body.user.postalCode
+		);
+		nameAndAddressType.setCountry(
+			req.body.country ? req.body.country : req.body.user.country
+		);
 
-    var arbSubscription = new ApiContracts.ARBSubscriptionType();
-    arbSubscription.setName(req.body.fname?req.body.fname:req.body.user.fname);
-    arbSubscription.setPaymentSchedule(paymentScheduleType);
-    arbSubscription.setAmount(req.body.amount);
-    arbSubscription.setTrialAmount(req.body.amount);
-    arbSubscription.setPayment(payment);
-    arbSubscription.setOrder(orderType);
-    arbSubscription.setCustomer(customer);
-    arbSubscription.setBillTo(nameAndAddressType);
-    arbSubscription.setShipTo(nameAndAddressType);
+		var arbSubscription = new ApiContracts.ARBSubscriptionType();
+		arbSubscription.setName(
+			req.body.fname ? req.body.fname : req.body.user.fname
+		);
+		arbSubscription.setPaymentSchedule(paymentScheduleType);
+		arbSubscription.setAmount(req.body.amount);
+		arbSubscription.setTrialAmount(req.body.amount);
+		arbSubscription.setPayment(payment);
+		arbSubscription.setOrder(orderType);
+		arbSubscription.setCustomer(customer);
+		arbSubscription.setBillTo(nameAndAddressType);
+		arbSubscription.setShipTo(nameAndAddressType);
 
-    var createRequest = new ApiContracts.ARBCreateSubscriptionRequest();
-    createRequest.setMerchantAuthentication(merchantAuthenticationType);
-    createRequest.setSubscription(arbSubscription);
+		var createRequest = new ApiContracts.ARBCreateSubscriptionRequest();
+		createRequest.setMerchantAuthentication(merchantAuthenticationType);
+		createRequest.setSubscription(arbSubscription);
 
-    // console.log(JSON.stringify(createRequest.getJSON(), null, 2));
-    // res.send(JSON.stringify(createRequest.getJSON(), null, 2));
-    // return
-    var ctrl = new ApiControllers.ARBCreateSubscriptionController(
-      createRequest.getJSON()
-    );
+		// console.log(JSON.stringify(createRequest.getJSON(), null, 2));
+		// res.send(JSON.stringify(createRequest.getJSON(), null, 2));
+		// return
+		var ctrl = new ApiControllers.ARBCreateSubscriptionController(
+			createRequest.getJSON()
+		);
 
-    ctrl.execute(function () {
-      var apiResponse = ctrl.getResponse();
+		ctrl.execute(function () {
+			var apiResponse = ctrl.getResponse();
 
-      var response = new ApiContracts.ARBCreateSubscriptionResponse(
-        apiResponse
-      );
+			var response = new ApiContracts.ARBCreateSubscriptionResponse(
+				apiResponse
+			);
 
-      res.send(JSON.stringify(response, null, 2));
+			res.send(JSON.stringify(response, null, 2));
 
-      if (response != null) {
-        if (
-          response.getMessages().getResultCode() ==
-          ApiContracts.MessageTypeEnum.OK
-        ) {
-          console.log("Subscription Id : " + response.getSubscriptionId());
-          console.log(
-            "Message Code : " + response.getMessages().getMessage()[0].getCode()
-          );
-          console.log(
-            "Message Text : " + response.getMessages().getMessage()[0].getText()
-          );
-        } else {
-          console.log("Result Code: " + response.getMessages().getResultCode());
-          console.log(
-            "Error Code: " + response.getMessages().getMessage()[0].getCode()
-          );
-          console.log(
-            "Error message: " + response.getMessages().getMessage()[0].getText()
-          );
-        }
-      } else {
-        console.log("Null Response.");
-      }
+			if (response != null) {
+				if (
+					response.getMessages().getResultCode() ==
+					ApiContracts.MessageTypeEnum.OK
+				) {
+					console.log("Subscription Id : " + response.getSubscriptionId());
+					console.log(
+						"Message Code : " + response.getMessages().getMessage()[0].getCode()
+					);
+					console.log(
+						"Message Text : " + response.getMessages().getMessage()[0].getText()
+					);
+				} else {
+					console.log("Result Code: " + response.getMessages().getResultCode());
+					console.log(
+						"Error Code: " + response.getMessages().getMessage()[0].getCode()
+					);
+					console.log(
+						"Error message: " + response.getMessages().getMessage()[0].getText()
+					);
+				}
+			} else {
+				console.log("Null Response.");
+			}
 
-      // callback(response);
-    });
-  });
-  if (require.main === module) {
-    createSubscription(function () {
-      console.log("createSubscription call complete.");
-    });
-  }
+			// callback(response);
+		});
+	});
+	if (require.main === module) {
+		createSubscription(function () {
+			console.log("createSubscription call complete.");
+		});
+	}
 
-  //   module.exports.createCustomerProfile = createCustomerProfile;
+	//   module.exports.createCustomerProfile = createCustomerProfile;
 };
