4a479837645543e5922fed69fd7ff2a5a2e313e0
abdul basit <muhammad.zubair3@hsrw.org>
2023-01-30 20:45:46 +0500

    changed isactive function-added lowercase matching

--- utils/account-activation-status.js
+++ utils/account-activation-status.js
@@ -2,29 +2,29 @@ const { DateTime } = require("luxon");
 
 //determines if the user's subscription is active
 const isActive = (user) => {
-  //if the user has a lifetime account, always return true
-  if (user.expirationDate === "never") return true;
-  //otherwise, determine if the current date is before the expiration date
-  const dt = DateTime.now();
-  const exp_dt = DateTime.fromISO(user.expirationDate);
-  let accountActive = false;
-  if (exp_dt.isValid) accountActive = dt < exp_dt;
-  else accountActive = false;
-  return accountActive;
+	//if the user has a lifetime account, always return true
+	if (user.expirationDate.toLowerCase() === "never") return true;
+	//otherwise, determine if the current date is before the expiration date
+	const dt = DateTime.now();
+	const exp_dt = DateTime.fromISO(user.expirationDate);
+	let accountActive = false;
+	if (exp_dt.isValid) accountActive = dt < exp_dt;
+	else accountActive = false;
+	return accountActive;
 };
 
 const isActiveAccountExec = (accountExec) => {
-  //first, make sure the account executive's account is not suspended
-  if (accountExec.suspended) return false;
-  //check that the accountExec is an accountExec, that their account is verified and that they have a stripeAccountID
-  //account executives do not need to have active user accounts to collect funds from referrals
-  if (
-    accountExec.isAccountExec &&
-    accountExec.stripeAccountID &&
-    accountExec.accountVerified
-  ) {
-    return true;
-  } else return false;
+	//first, make sure the account executive's account is not suspended
+	if (accountExec.suspended) return false;
+	//check that the accountExec is an accountExec, that their account is verified and that they have a stripeAccountID
+	//account executives do not need to have active user accounts to collect funds from referrals
+	if (
+		accountExec.isAccountExec &&
+		accountExec.stripeAccountID &&
+		accountExec.accountVerified
+	) {
+		return true;
+	} else return false;
 };
 
 module.exports.isActive = isActive;
